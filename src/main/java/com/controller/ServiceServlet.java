9aabc4fc94a241e2917bf809def532dfe897851d
S62270 <s62270@ocean.umt.edu.my>
2023-06-19 18:22:22 +0800

    pushing problems

--- src/main/java/com/controller/ServiceServlet.java
+++ src/main/java/com/controller/ServiceServlet.java
@@ -12,7 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
 
-@WebServlet("/")
 public class ServiceServlet extends HttpServlet {
 
     private ServicesDAO serviceDao;
